d179c489d01dbab49b3e1efb34353a0da4acc5f0
Rohde Felipe <fr.rohde@gmail.com>
2016-12-29 23:27:27 -0200

    Cities median temperature calculation.

--- _source/source/_scripts/main.js
+++ _source/source/_scripts/main.js
@@ -66,34 +66,73 @@ $(() => {
   } );
 
   ///for testing
-  // var data = {
-  //     "email": "fr.rohde@gmail.com",
-  //     "password": '',
-  //     "fullName": '',
-  //     "birthDate": '',
-  //     "zipCode": '',
-  //     "streetName": '',
-  //     "number": '',
-  //     "complement": '',
-  //     "neighbourhood": '',
-  //     "country": '',
-  //     "state": '',
-  //     "city": ''
-  //   };
+   var data = {
+       "email": "fr.rohde@gmail.com",
+       "password": '',
+       "fullName": '',
+       "birthDate": '',
+       "zipCode": '',
+       "streetName": '',
+       "number": '',
+       "complement": '',
+       "neighbourhood": '',
+       "country": '',
+       "state": '',
+       "city": ''
+     };
 
-  // xhr.Post({data: JSON.stringify(data)}, function (data) {
-  //     var notification = new Notification({cssMap: css});
-  //     notification.Push(data.msg, data.status);
+   xhr.Post({data: JSON.stringify(data)}, function (data) {
+       var notification = new Notification({cssMap: css});
+       notification.Push(data.msg, data.status);
 
-  //     if (data.status === 200 ) {
-  //       xhr.Post({data: data.data, url: 'http://www.improving.com.br/api/test/hits-by-browser' }, function (response) {
+       if (data.status === 200 ) {
+         xhr.Post({data: data.data, url: 'http://www.improving.com.br/api/test/city-temperatures' }, function (response) {
 
-  //         let graph =  new Graph({cssMap: css});
-  //         //todo: create var or receive a param
-  //         $('#results-section').html(graph.Render(response.data));
-  //       });
-  //     }
+            let citiesData = JSON.parse(response.data);
+            let citiesTemperatureMedian = [];
 
-  //   });
+            for (let i = citiesData.length - 1; i >= 0; i--) {
+
+              let lastMonth;
+
+              let temp = new Array();
+              let dayCount = 0;
+              let total = 0;
+
+              for (let j = citiesData[i].data.length - 1; j >= 0; j--) {
+
+                let mes = citiesData[i].data[j][0].split('-')[1];
+                let dia = citiesData[i].data[j][0].split('-')[0];
+
+                if(!temp[mes])
+                  temp[mes] = [];
+
+                total += parseInt(citiesData[i].data[j][1], 10);
+
+                if(lastMonth !== mes) {
+                  temp[mes]['media'] = (total / dayCount).toFixed(1);
+                  dayCount = 0;
+                  total = 0;
+                }
+
+                dayCount ++;
+                lastMonth = mes;
+              }
+              citiesTemperatureMedian[citiesData[i].name] = temp;
+
+            }
+
+            console.log(citiesTemperatureMedian);
+
+            //console.log(months);
+
+
+           //let graph =  new Graph({cssMap: css});
+           //todo: create var or receive a param
+           //$('#results-section').html(graph.Render(response.data));
+         });
+       }
+
+     });
 
 });
